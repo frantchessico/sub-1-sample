2fa94516ca481372e661e509fbcae3c0f3c9922f
Francisco Inoque <franciscoinoque@MacBook-Air-de-Francisco.local>
2024-08-01 01:34:38 +0200

    add port

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-
-
-
 import dotenv from 'dotenv';
-dotenv.config();
-
-
+dotenv.config()
+import express from 'express';
+import morgan from 'morgan';
 import { createNewsletter } from './service/newsletter.service';
 import { connection } from './configs/mongodb';
 import { sub } from './configs/subscriber';
 
 connection();
+
+
+const app = express();
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+const port = process.env.PORT || 2600;
+
+
+
+
+app.get('/', (_, res) => {
+  return res.send(`<h1>Welcome to Subscriber 1 Sample`)
+})
+
 
 
 
@@ -21,3 +34,5 @@ sub.subscribe('newsletter', ['pubsub'], async (message: string) => {
   await createNewsletter(msg.name, msg.email);
 })
 
+
+app.listen(port, () => console.log(`Server on http://localhost:${port}`))
